4255185bb9443cc48772e01ad6456b130a50215d
chirag1210 <chirag.chaudhari7@gmail.com>
2017-10-11 11:39:29 +0530

    dfhd

---
+++ app/src/main/java/katta/chirag/in/katta/SplashActivity.java
@@ -0,0 +1,32 @@
+package katta.chirag.in.katta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        // Using handler with postDelayed called runnable run method
+
+        new Handler().postDelayed(new Runnable() {
+
+            // Using handler with postDelayed called runnable run method
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 5*1000);
+    }
+}
